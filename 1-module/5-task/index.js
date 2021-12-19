ca7e83a8b3307b8c657f828f07723df97aca3585
TimofeevAnton1980 <Maestroat@yandex.ru>
2021-12-19 23:47:15 +0300

    lesson-2

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
   // ваш код...
+  let strLenght = str.length;
+  if (strLenght > maxlength) {
+    str = str.slice(0, maxlength-1);
+    str = str + "…";
+    return str;
+  }
+  return str;
 }
