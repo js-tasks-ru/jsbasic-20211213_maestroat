7bc5a568a59b51f125a7b06c2ff7601251a82d39
TimofeevAnton1980 <Maestroat@yandex.ru>
2021-12-26 23:43:08 +0300

    lesson-3

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,9 @@
 function isEmpty(obj) {
   // ваш код...
+  let sum = false;
+  for (let key in obj) {
+    if (key in obj === false) {sum = sum || false;} else {sum = sum || true;}
+
+  }
+  return !sum;
 }
